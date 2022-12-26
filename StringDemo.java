1fd4d8c78da18c0ae619acc880b7ac22391b61a5
kanniyappan143 <119713241+kanniyappan143@users.noreply.github.com>
2022-12-26 09:12:11 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,18 @@
+package nonpreemptive;
+
+public class StringDemo {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		  
+		 /*String data type example(string value store in only on declare format)
+		 *  	data type  variable ="value"
+		 */
+		 String Name =" Name is kannoyappan Bsc Computer Science ";
+		 String Qualification=" 12+3 Bsc Cs";
+		 
+		 System.out.println("Whats your name"+Name);
+		 System.out.println("Your Qualification?"+Qualification);
+	}
+
+}
