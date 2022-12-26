a1b43f386c4003520a0e8fa4112589c59352b0e2
kanniyappan143 <119713241+kanniyappan143@users.noreply.github.com>
2022-12-26 09:12:38 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+package nonpreemptive;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Array stores more data in a order 
+		 * Important Rules of Array
+		 * once array declare we cannot be do increase and decreased process  at size of array
+		 * call any value in array with use in index(example System.out.println("Number"+Number[2]); ->output 3 refer below program*/
+		int Numbers[]= {1,2,3,4};
+		System.out.println("Number"+Numbers[2]);
+		
+		char alfabet[]={'a','b','c'};
+		System.out.println("Alfabet:"+alfabet[2]);
+		
+		String Name []= {"My Name is kanniyappan"};
+		System.out.println("Whats your name:"+Name [0]);
+		
+	}
+
+}
